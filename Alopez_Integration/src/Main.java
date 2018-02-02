4914a6a74b99846ee2d6ea3494150a7489cfa9ef
Alexander Lopez <Alexander Lopez@10.100.249.96>
2018-02-01 19:16:10 -0500

    Decision statement

--- Alopez_Integration/src/Main.java
+++ Alopez_Integration/src/Main.java
@@ -1,48 +1,83 @@
 import java.util.Scanner;
 
 //Alexander Lopez
-//My project that integrates all the skills I have learned
-// sysout Ctrl+space
+//This is my project that integrates all the skills I have learned
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		//System.out.println("Hello World!");
+		String myName = "Alex";
+		int myAge = 19;
+		//introduction
+		System.out.println("Hello and welcome to this project!");
 		
+		//Asks user for name
 		System.out.println("What is your name?");
-		String userName = scan.nextLine();
-		System.out.println(userName);
+		String name = myName; //scan.nextLine();
 		
-		System.out.println("Enter x1"); //Prompt
-		int x1 = scan.nextInt();
-		System.out.println(x1);
+		//Responds to user's name and says it back
+		System.out.println("Oh, so your name is "
+				+ name  + "? Nice to meet you, " + name + "."); 
 		
-		System.out.println("Enter rate"); //Prompt
-		double rate = scan.nextDouble();
-		System.out.println(rate);
+		//Asks user for their age
+		System.out.println("How old are you, " + name 
+				+ "? If I may ask?");
+		int age = myAge; //scan.nextInt();
 		
+		//Responds to the user's age and repeats it back
+		System.out.println("So you are " + age + " years old."
+				+ " How wonderful!");
+		
+		//Asks if the user knows about variables
+		System.out.println("Did you know, " + name + " that a variable is "
+				+ "like a box in memory that holds specific data "
+				+ "depending on what kind of box it is?"); 
+		System.out.println("Yes or no?");
 		scan.nextLine();
+		String answerYS = scan.nextLine();
 		
-		System.out.println("What is your title?");
-		String title = scan.nextLine();
-		System.out.println(title);
+		//Sets the user's response to all lower case
+		answerYS = answerYS.toLowerCase();
+				
+		//A switch statement to determine outcome based on user response
+		switch (answerYS) {
+		  case "yes":
+		    System.out.println("Oh. That's great then!");
+		    break;
+		    
+		  case "no":
+		    System.out.println("Really? Well there are many data types"
+	          + " in Java. For example your name is stored in"
+	          + " a data type called a String, but that"
+	          + " isn't the only data type there is. There are"
+	          + " also int's (Integers), double's, floats, longs, shorts,"
+	          + " bytes, booleans, and chars (characters) in the language too!");
+		    break;
+		    
+		  default:
+		      System.out.println("You didn't input a vaild response.");
+	    
+		}
 		
-		Dog spot = new Dog();
-		spot.size = 30.5;
-		spot.breed = "German Shepard";
-		System.out.println(spot.breed);
+		//String = An object in Java that allows you to store words and letters
+		//int = A data type that allows you to store whole numbers (integers)
+		//double = A data type that allows you to store numbers with decimals
+		//float = A data type similar to double, but can hold larger values
+		//long = A data type that stores whole numbers larger than int can hold
+		//short = A data type that has a smaller range of whole numbers compared 
+		  //to int
+		//byte = A data type that has a smaller range of whole numbers compared
+		  //to short
+		//char = A data type that can only hold one character (letter or symbol)
+		//boolean = A data type with only two values (true or false)
 		
-		Dog spuds = new Dog();
-		spuds.size = 40.3;
-		spuds.breed = "Bull Dog";
+		
+			
 	}
 
 }
 
-class Dog{
-	double size;
-	String breed;
-}
+
 //Objects are instances of a class...like a house
 //Class is code...like a blueprint
 //Only ever have one project open in Eclipse applied
