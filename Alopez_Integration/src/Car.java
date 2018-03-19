2ea1d304e69e0e1c72014bc7fda7b529f11ad652
Alexander Lopez <Alexander Lopez@10.0.0.114>
2018-03-18 21:56:25 -0400

    Integration PSI 2

--- Alopez_Integration/src/Car.java
+++ Alopez_Integration/src/Car.java
@@ -1,42 +1,66 @@
 //Alexander Lopez
 public class Car {
-  
-  private int speed;
+
+  private int topSpeed;
   private String make;
+  static private int currentSpeed;
+
+  public Car() {
+    topSpeed = 0;
+    make = "Unknown";
+    currentSpeed = 50;
+  }
+
+  public Car(int s) {
+    topSpeed = s;
+    make = "No make found";
+    currentSpeed = 50;
+  }
+
+  public Car(String m) {
+    topSpeed = 0;
+    make = m;
+    currentSpeed = 50;
+  }
+
+  public Car(int s, String m) {
+    topSpeed = s;
+    make = m;
+    currentSpeed = 50;
+  }
+  
+  public Car(int s, String m, int c) {
+    topSpeed = s;
+    make = m;
+    currentSpeed = c;
+  }
 
   public void setSpeed(int s) {
-    speed = s;
+    topSpeed = s;
   }
+
   public int getSpeed() {
-    return speed;
+    return topSpeed;
   }
 
   public void setMake(String m) {
-    make = m; 
+    make = m;
   }
+
   public String getMake() {
     return make;
   }
   
-  public Car() {
-    speed = 0;
-    make = "Unknown";
+  static public void setCurrentSpeed(int c) {
+    currentSpeed = c;
   }
   
-  public Car(int s) {
-    speed = s;
-    make = "No make found";
-  }
-  
-  public Car(String m) {
-    speed = 0;
-    make = m;
-  }
-  
-  public Car(int s, String m) {
-    speed = s;
-    make = m;
+  static public int getCurrentSpeed() {
+    return currentSpeed;
   }
 
+  static public void applyBrake() {
+    currentSpeed--;
+  }
 
 }
--- Alopez_Integration/src/Main.java
+++ Alopez_Integration/src/Main.java
@@ -6,28 +6,23 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    // Variables
-    // String myName = "Alex";
-    // int myAge = 19;
 
     // introduction
-    /*System.out.println("Hello and welcome to my integration project!");
+    System.out.println("Hello and welcome to my integration project!");
 
     // Asks user for name
     System.out.println("What is your first and last name?");
-    // String name = myName;
     String firstName = scan.next();
     String lastName = scan.next();
     scan.nextLine();
     String name = firstName + " " + lastName;
 
     // Responds to user's name and says it back
-    System.out.println("Oh, so your name is " + name + "? Nice to meet you, "
+    System.out.println("Oh, so your name is " + name + "?\nNice to meet you, "
         + firstName + ".");
 
     // Asks user for their age
-    System.out.println("How old are you, " + firstName + "? If I may ask?");
-    // int age = myAge;
+    System.out.println("How old are you, " + firstName + "?\nIf I may ask?");
     int age = scan.nextInt();
     scan.nextLine();
 
@@ -35,9 +30,9 @@ public class Main {
     System.out.println("So you are " + age + " years old." + " How wonderful!");
 
     // Asks if the user knows about variables
-    System.out.println("Did you know, " + firstName + " that a variable is "
-        + "like a box in memory that holds specific data "
-        + "depending on what kind of box it is?");
+    System.out.println("Did you know, " + firstName + " that a variable is"
+        + "\nlike a box in memory that holds specific data"
+        + "\ndepending on what kind of box it is?");
     System.out.println("Yes or no?");
     // String answerYS = "yes";
     String answerYS = scan.nextLine();
@@ -53,15 +48,15 @@ public class Main {
 
       case "no":
         System.out.println("Really? Well there are many data types"
-            + " in Java. For example your name is stored in"
-            + " a data type called a String, but that"
-            + " isn't the only data type there is. There are"
-            + " also int's (Integers), double's, floats, longs, shorts,"
-            + " bytes, booleans, and chars (characters) in the language too!");
+            + "\nin Java. For example your name is stored in"
+            + "\na data type called a String, but that"
+            + "\nisn't the only data type there is. There are"
+            + "\nalso int's (Integers), double's, floats, longs, shorts,"
+            + "\nbytes, booleans, and chars (characters) in the language too!");
         break;
 
       default:
-        System.out.println("You didn't input a vaild response.");
+        System.out.println("You didn't input a valid response.");
 
     }
 
@@ -81,14 +76,14 @@ public class Main {
     GetIntials userInitials = new GetIntials();
 
     // assigns the returned value of the personInitials method to initials
-    // ----------------------------Call----------------Arguments
-    // -----------------------------|----------------------|
-    // -----------------------------V----------------------V
+    // -----------------------------Call----------------Arguments
+    // ------------------------------|----------------------|
+    // ------------------------------V----------------------V
     String initials = userInitials.personInitials(firstName, lastName);
 
     // Outputs the user's first and last name as well as their initials
     System.out.println("So you are " + firstName + " " + lastName + "."
-        + " That means your initials would be " + initials + "!");
+        + "\nThat means your initials would be " + initials + "!");
 
     // Prompts an integer input from the user
     System.out.println("Can you enter a number between 1 and 100 that"
@@ -115,7 +110,7 @@ public class Main {
 
     // String confusedAnswer = "yes";
     String confusedAnswer = scan.nextLine();
-    confusedAnswer.toLowerCase();
+    confusedAnswer = confusedAnswer.toLowerCase();
 
     // The condition for the later if-else statement
     byte ifCondition;
@@ -133,7 +128,7 @@ public class Main {
         break;
 
       default:
-        System.out.println("Well I'm gonna tell you a story anyways.");
+        System.out.println("Well I'm going tell you a story anyways.");
         ifCondition = 1;
         break;
 
@@ -142,12 +137,12 @@ public class Main {
     // Gives a response based on what the user previously answered
     if (ifCondition == 1) {
       System.out.println("What happened was integer division."
-          + " Integer division is special because in Java if two"
-          + " integers are being divided then the result will ALWAYS"
-          + " be an integer as well. Which in the case of values that"
-          + " end up being in between two integers then the program"
-          + " will truncate the number leaving only the number to"
-          + " the left of the decimal.");
+          + "\nInteger division is special because in Java if two"
+          + "\nintegers are being divided then the result will ALWAYS"
+          + "\nbe an integer as well. Which in the case of values that"
+          + "\nend up being in between two integers then the program"
+          + "\nwill truncate the number leaving only the number to"
+          + "\nthe left of the decimal.");
     } else {
       System.out.println("Well I'm glad to meet a fellow programmer!");
       System.out.println("Well let's start on non-integer division.");
@@ -155,7 +150,7 @@ public class Main {
 
     // Prompts the user to enter the different numbers now
     System.out.println("Alright, let us try some normal division."
-        + " Please enter a new whole number between 1-100.");
+        + "\nPlease enter a new whole number between 1-100.");
     int divNum1 = scan.nextInt();
 
     // Prompts the user to enter the second number to divide
@@ -171,12 +166,12 @@ public class Main {
 
     // Explains why we have a decimal number now
     System.out.println("We were now able to get a decimal answer"
-        + " because within the code the second integer was treated like"
-        + " a double through casting!");
+        + "\nbecause within the code the second integer was treated like"
+        + "\na double through casting!");
 
     // Tells the user to enter 4 numbers to have math operations applied to
     System.out.println("Now let's do something crazy. Please enter four"
-        + " different integers between 1 and 100 seperated by a space.");
+        + " different integers between 1 and 100 separated by a space.");
     int firstNum = scan.nextInt();
     int secondNum = scan.nextInt();
     int thirdNum = scan.nextInt();
@@ -185,9 +180,9 @@ public class Main {
 
     // Tells the user what operations we will be doing with the numbers
     System.out.println("Now we are going to add the first two numbers,"
-        + " the sum being multiplied by the third, the product being"
-        + " subtracted by the fourth, and finally the difference will"
-        + " be divided by the first number.");
+        + "\nthe sum being multiplied by the third, the product being"
+        + "\nsubtracted by the fourth, and finally the difference will"
+        + "\nbe divided by the first number.");
 
     // Math calculation
     double complexMath = (((firstNum + secondNum) * thirdNum) - fourthNum)
@@ -212,7 +207,7 @@ public class Main {
     System.out.println("So you drive a " + userCar.getMake()
         + " and its top speed is " + userCar.getSpeed());
 
-    // do-while loop for the user to hit x to continue the program
+    // do-while loop for the user to hit 5 to continue the program
     int keypress;
     do {
       System.out.println("Press 5 to continue.");
@@ -222,12 +217,12 @@ public class Main {
 
     // Prompts user to enter a certain amount of numbers
     System.out.println(
-        "Please enter five different integers seperated" + " by spaces.");
+        "Please enter five different integers separated" + " by spaces.");
 
     // Makes an array of integers that holds up to 5 elements
     int numbers[] = new int[5];
 
-    // A for loop to assign all integers typed in to the different
+    // A for loop to assign all integers typed in, to the different
     // elements of the numbers array
     for (int counter = 0; counter < numbers.length; counter++) {
       numbers[counter] = scan.nextInt();
@@ -238,134 +233,201 @@ public class Main {
 
     // Lets the user know what is going to happen next
     System.out.println("Now lets see what is the smallest number you entered."
-        + " Press enter to continue.");
+        + "\nPress enter to continue.");
     scan.nextLine();
 
     int numSum = numbers[0];
     int largest = numbers[0];
     int smallest = numbers[0];
-    
+
     for (int i = 1; i < numbers.length; i++) {
       numSum += numbers[i];
 
       if (numbers[i] > largest) {
         largest = numbers[i];
-      }
-
-      else if (numbers[i] < smallest) {
+      } else if (numbers[i] < smallest) {
         smallest = numbers[i];
       } else {
         System.out.println("Loading");
       }
     }
-    
-    //Returns smallest number
+
+    // Returns smallest number
     System.out.println("Done!");
     System.out.println("The smallest number you entered was... " + smallest);
-    
-    //Returns largest number
-    System.out.println("Now lets see what the biggest number is. Press enter"
-        + " to continue.");
+
+    // Returns largest number
+    System.out.println("Now lets see what the biggest number is.\n"
+        + "Press enter to continue.");
     scan.nextLine();
-    
+
     System.out.println("Loading...");
     System.out.println("Done!");
     System.out.println("The biggest number you entered was... " + largest);
-    System.out.println("Alright not lets calculate the sum of all these"
-        + " numbers. Press enter to continue.");
-    scan.nextLine();
-    
     System.out.println("Alright now lets calculate the sum of all these"
-        + " numbers");
+        + " numbers.\nPress enter to continue.");
+    scan.nextLine();
+
     System.out.println("Loading...");
     System.out.println("Done!");
-    System.out.println("The total sum of the numbers you entered is"
-        + " " + numSum);
-    
-    //Going prompts user to enter a word
+    System.out.println("The total sum of the numbers you entered is " + numSum);
+
+    // Going prompts user to enter a word
     System.out.println("Let's try something a little more complicated."
-        + " Please enter your favorite word.");
+        + "\nPlease enter your favorite word.");
     String favWord = scan.nextLine();
-    
-    //Let's the user know what is going to take place
+
+    // Let's the user know what is going to take place
     System.out.println("Now let's get on with the magic trick.\n"
         + "Tell me a letter you would like me to search for\n"
-        + "and if it is in your favorite word.\n"
-        //+ "then I will tell\n"
-        //+ "you where the first time that letter occurs in the word.\n"
+        + "and if it is in your favorite word\n" + "then I will let you know.\n"
         + "If it is not in the word then I will let you know.");
-    
-    //prompts the user to enter a letter
-    System.out.println("Please enter was letter you would like\n"
-        + "me to search.");
+
+    // prompts the user to enter a letter
+    System.out
+        .println("Please enter was letter you would like\n" + "me to search.");
     String letToSearch = scan.nextLine();
-    
-    //fake load time
+
+    // fake load time
     System.out.println("Loading...");
-    
-    //turns both favWord and letToSearch into all lower case
+
+    // turns both favWord and letToSearch into all lower case
     favWord = favWord.toLowerCase();
     letToSearch = letToSearch.toLowerCase();
-    //System.out.println(favWord + " " + letToSearch);
-    //scan.nextLine();
-    
-    //converts both favWord and letToSearch into arrays or chars
+    // System.out.println(favWord + " " + letToSearch);
+    // scan.nextLine();
+
+    // converts both favWord and letToSearch into arrays of chars
     char[] favChars = favWord.toCharArray();
     char[] charToSearch = letToSearch.toCharArray();
-    
-    //Loop to search through the favChars array to check if the 
-    //charToSearch is in the favChars array
+
+    // Loop to search through the favChars array to check if the
+    // charToSearch is in the favChars array
     boolean found = false;
     int index = 0;
-    while(found == false && index < favChars.length) {
-      if(favChars[index] == charToSearch[0]) {
-        //int placeOfChar = indexOf(charToSearch[0], 0);
+    while (found == false && index < favChars.length) {
+      if (favChars[index] == charToSearch[0]) {
+        // int placeOfChar = indexOf(charToSearch[0], 0);
         System.out.println("I have found a(n) " + letToSearch + ".");
-        //System.out.println("It was at position " + placeOfChar + "." );
+        // System.out.println("It was at position " + placeOfChar + "." );
         found = true;
       } else {
         System.out.println("Loading...");
         index++;
       }
-      
+
     }
-    
-    if(found == false) {
+
+    if (found == false) {
       System.out.println("Your letter was not found in your word.");
     }
-    */
-    
+
     System.out.println("Press enter to continue");
     scan.nextLine();
-    
-    //Prompts user to what is going to happen
-    System.out.println("Now we are gonna do a little bit of math.\n"
+
+    // Prompts user to what is going to happen
+    System.out.println("Now we are going do a little bit of math.\n"
         + "I will give you the product of the following 5 numbers...");
-    
-    //Does a for-each loop to print out all numbers in the arr array 
-    int[] arr = {1, 2, 3, 4, 5};
-    for(int loop : arr) {
+
+    // Does a for-each loop to print out all numbers in the arr array
+    int[] arr = { 1, 2, 3, 4, 5 };
+    for (int loop : arr) {
       System.out.print(loop + " ");
     }
-    
-    System.out.println("\nNow to multiply them togther.\n"
-        + "Press enter to continue");
+
+    System.out.println(
+        "\nNow to multiply them together.\n" + "Press enter to continue");
     scan.nextLine();
-    
-    //Calculates the total product
+
+    // Calculates the total product
     int numProduct = arr[0];
-    for(int k = 1; k < arr.length; k++) {
-      numProduct *= arr[k];
+    for (int k = 1; k < arr.length; k++) {
+      numProduct = arr[k];
     }
     System.out.println("Loading...\nDone!");
     System.out.println("The total product is " + numProduct + ".");
-    
+
+    // Gives a scenario to the user
+    // This uses static fields and methods. Static means that the fields
+    // and methods belong to the class itself and not to an object
+    System.out.println("Quick! You are on the highway and a cop is coming\n"
+        + "up behind you. Press enter to check your current speed");
+    scan.nextLine();
+
+    // returns the current speed
+    System.out.println("You look at your speedometer and you are going "
+        + Car.getCurrentSpeed() + "mph. Would you like to change your speed?");
+
+    // Evaluates the users choice
+    System.out.println("Yes or no?");
+    String choice = scan.nextLine();
+    int newSpeed;
+    choice = choice.toLowerCase();
+    if (choice.equals("yes")) {
+      System.out.println("Enter what speed you would like to go");
+      newSpeed = scan.nextInt();
+      Car.setCurrentSpeed(newSpeed);
+      System.out.println("OK. You are now going " + Car.getCurrentSpeed()
+          + "mph. Now lets see if you evade the cops..." + "\nLoading...");
+
+      // Determines if user evades
+      if (Car.getCurrentSpeed() > 75) {
+        System.out.println(
+            "Sorry... You were going too fast and were pulled" + " over.");
+      } else if (Car.getCurrentSpeed() < 65) {
+        System.out.println(
+            "Sorry... You were going too slow and were pulled" + " over.");
+      } else {
+        System.out.println("Good job. You stayed within the speed limit.");
+      }
+
+    } else {
+      System.out.println("Good choice. You are already going the speed limit"
+          + "\nsince there is construction currently going on.");
+    }
+
+    System.out.println("Press enter to continue");
+    scan.nextLine();
+
+    // Reversing a given input of the user
+    System.out.println(
+        "Its time for more magic. " + "\nPlease enter a short phrase.");
+    String phrase = scan.nextLine();
+    StringBuilder reverse = new StringBuilder(phrase);
+    System.out.println("Here is your phrase backwards: " + reverse.reverse());
+
+    System.out.println("Press enter to continue");
+    scan.nextLine();
+
+    // Exercise in the .append() method
+    System.out.println("Please enter an adjective...");
+    String adj = scan.nextLine();
+    System.out.println("Please enter a noun...");
+    String noun = scan.nextLine();
+    System.out.println("Loading...");
+    StringBuilder sbAdj = new StringBuilder(adj);
+    System.out.println("Here is the result: " + sbAdj.append(" " + noun));
+
+    System.out.println("Press enter to continue");
+    scan.nextLine();
+
+    // Using the .deleteCharAt() method
+    System.out.println("Please enter a word");
+    String word = scan.nextLine();
+    System.out
+        .println("Now enter an integer between 0 and " + (word.length() - 1));
+    int charPlace = scan.nextInt();
+    scan.nextLine();
+    System.out.println("Loading...");
+    StringBuilder newWord = new StringBuilder(word);
+    newWord = newWord.deleteCharAt(charPlace);
+    System.out.println("Here is your new word: " + newWord);
+
     // Let's the user know that the program has come to an end
     System.out.println("Well this is the end of this program."
         + " Thanks for playing with me. Bye!");
     scan.close();
   }
- 
 
 }
 
